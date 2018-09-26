f1508ed077b69c9740328a43aa9c73ef24d5297c
matias6942 <matias.zamora@ing.uchile.cl>
2018-09-26 17:18:48 -0300

    Frame resized with his timestamp rounded to 2 decimals

--- featuresExtraction.py
+++ featuresExtraction.py
@@ -2,29 +2,31 @@
 import numpy as np
 import cv2
 
-def nFramesFromVideo(videoFile, n):
-    """Return frames indexes from a video file every n frames with its timestamps 
+def nFramesFromVideo(videoFile, frameStride, width, height):
+    """Return frames resized to width and heigth from a video file every n frames with its timestamps 
     in miliseconds"""
 
     print("\n" + "Frames Extraction has Started...")
     capture = cv2.VideoCapture(videoFile)
-    frameCounter = 0
-"""
-    totalFrames = capture.get(7)
+    
+    totalFrames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
     print("This video file contains " + str(int(totalFrames)) + " frames.")
-"""
+    
+    frameCounter = 0
     while(capture.isOpened()):
         success, frame = capture.read()
         if success:
             frameCounter +=1
-            if(frameCounter % n == 0):
-                
+            if(frameCounter % frameStride == 0):
+                frame = cv2.resize(frame, (width, height))
                 cv2.imshow('Video', frame)
+                
                 frameTimestamp = "{0:.2f}".format(round(capture.get(0),2))
-                frameIndex = int(capture.get(1))
+                frameIndex = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
                 print(str(frameIndex) + " " + frameTimestamp)
 
-                # cv2.waitKey(25) for normal speed
+                # Normal Speed cv2.waitKey(25) 
+                # Max Speed     cv.waitKey(1)
                 if cv2.waitKey(1) & 0xFF == ord('q'):
                     break
         else:
@@ -39,11 +41,14 @@ print("\n" + "Executing featuresExtraction.py..." + "\n")
 
 #videoPath = input("Hi! Enter the video file name to process please: ")
 #commFolder = input("\n" + "Now,  enter the folder name that contains the commercials to look for: ")
-nFramesFromVideo('../CC5213-Tarea1/comerciales/ballerina.mpg', 4)
 
+# Set frame stride
+frameStride = 4
 
+# Resize to 10x10 to reduce quantity of pixels at every frame
+width = 1080
+height = 720
 
-
-
+nFramesFromVideo('../CC5213-Tarea1/comerciales/ballerina.mpg', frameStride, width, height)
         
         
