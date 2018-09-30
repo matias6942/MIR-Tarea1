5bd2e02736d8a58880dec8a2cb79ebb0535659db
matias6942 <matias.zamora@ing.uchile.cl>
2018-09-30 20:47:11 -0300

    Sobel Filter Working on Frames

--- featuresExtraction.py
+++ featuresExtraction.py
@@ -1,8 +1,8 @@
 
+
+import numpy as np
 import cv2 
 import os
-import easygui
-
 
 def openVideo(filename = None):
     """Given a filename, checks if the file exist and is accesible 
@@ -25,12 +25,27 @@ def openVideo(filename = None):
     
     return capture;
 
-def sobelFilter(filename, sobel_threshold):
-    """Given a filename, applies a Sobel filter with a threshold
-    to the file and returns .............
+def showFrame(windowName, frame, absValue=False, scaleMin0Max255=False):
+    """Names the window to show a given frame via GUI. Offers the options 
+    to take the absolute value of the frame and normalize it if you want."""
+    
+    if absValue:
+        frame_abs = np.abs(frame)
+    else:
+        frame_abs = frame
+    
+    if scaleMin0Max255:
+        frame_norm = cv2.normalize(frame_abs, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+    else:
+        frame_norm = frame_abs
+    cv2.imshow(windowName, frame_norm)
+
+def sobelFilter(filename, sobel_threshold, frameStride):
+    """Given a filename and a frameStride, applies a Sobel filter 
+    with a sobel_threshold with a specified frameStride 
+    beginning from the first frame of the capture.
     """
     capture = openVideo(filename)
-    frameStride = 4
     
     # DEBUG
     totalFrames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
@@ -51,10 +66,29 @@ def sobelFilter(filename, sobel_threshold):
 
                 # Frame Processing
 
-                # Wait for a key
+                ## Resize frame
+                frame = cv2.resize(frame, (1080, 720))
+
+                ## frame to gray scale (improves filter preformance) 
+                grayFrame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                
+                ## grayframe filtered by the Sobel Filter
+                sobelX = cv2.Sobel(grayFrame, ddepth=cv2.CV_32F, dx=1, dy=0, ksize=3)
+                sobelY = cv2.Sobel(grayFrame, ddepth=cv2.CV_32F, dx=0, dy=1, ksize=3)
+
+                ## Gradient Magnitude or Approximation for Gradient Magnitude (Only Choose One!)
+                
+                #gradientAbs = np.sqrt(np.square(sobelX) + np.square(sobelY))
+                gradientAbs = np.abs(sobelX) + np.abs(sobelY)
+
+                # Apply Sobel Threshold
+                retval, borders = cv2.threshold(gradientAbs, thresh=sobel_threshold, maxval=255, type=cv2.THRESH_BINARY)
+
+                showFrame("Filtered Frame", borders, scaleMin0Max255=True)
+                
                 # Normal Speed cv2.waitKey(25) 
                 # Max Speed     cv.waitKey(1)
-                if cv2.waitKey(25) & 0xFF == ord('q'):
+                if cv2.waitKey(45) & 0xFF == ord('q'):
                     break
         
         else:
@@ -69,6 +103,8 @@ def sobelFilter(filename, sobel_threshold):
 videoPath = '../CC5213-Tarea1/comerciales/ballerina.mpg'
 
 sobel_threshold = 150
+frameStride = 4
 
-sobelFilter(videoPath, sobel_threshold)
+sobelFilter(videoPath, sobel_threshold, frameStride)
 print("FIN")
+
