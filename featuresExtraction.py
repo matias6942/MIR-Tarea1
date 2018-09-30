27d77ea6baf9fbc4725ae38bf59e261a2d533b05
matias6942 <matias.zamora@ing.uchile.cl>
2018-09-30 19:07:36 -0300

    featuresExtraction with non implemented Sobel filter

--- featuresExtraction.py
+++ featuresExtraction.py
@@ -1,70 +1,74 @@
 
-import numpy as np
-import cv2
-import time
+import cv2 
+import os
+import easygui
 
-def nFramesFromVideo(videoPath, frameStride, width, height):
-    """Return frames resized to width and heigth from a video file every n frames with its timestamps 
-    in miliseconds"""
 
-    print("\n" + "Frames Extraction has Started...")
-    capture = cv2.VideoCapture(videoPath)
+def openVideo(filename = None):
+    """Given a filename, checks if the file exist and is accesible 
+    and returns the capture."""
+    if filename is None:
+        filename = 1
+    elif filename.isdigit():
+        filename = int(filename)
+
+    if isinstance(filename, int):
+        print("Activating integrated webcam {}".format(filename))
+        capture = cv2.VideoCapture(filename)
+
+    if(os.path.isfile(filename)):
+        print("Opening file {}".format(filename))
+        capture = cv2.VideoCapture(filename)
+
+    if not capture.isOpened():
+        raise Exception("Error opening file {}".format(filename))
     
+    return capture;
+
+def sobelFilter(filename, sobel_threshold):
+    """Given a filename, applies a Sobel filter with a threshold
+    to the file and returns .............
+    """
+    capture = openVideo(filename)
+    frameStride = 4
+    
+    # DEBUG
     totalFrames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
     print("This video file contains " + str(int(totalFrames)) + " frames.")
-    
-    frameCounter = 0
+
+    frameCount = 0
     while(capture.isOpened()):
         success, frame = capture.read()
         
-
-        if success:
-            frameCounter +=1
-            if(frameCounter % frameStride == 0):
-                frame = cv2.resize(frame, (width, height))
-                cv2.imshow('Video', frame)
+        if(success):
+            frameCount+=1
+            if(frameCount == 1 or frameCount % frameStride == 0):
                 
+                # DEBUG
                 frameTimestamp = "{0:.2f}".format(round(capture.get(0),2))
                 frameIndex = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
                 print(str(frameIndex) + " " + frameTimestamp)
 
-                #return "Hola!"
+                # Frame Processing
 
+                # Wait for a key
                 # Normal Speed cv2.waitKey(25) 
                 # Max Speed     cv.waitKey(1)
-                if cv2.waitKey(1) & 0xFF == ord('q'):
+                if cv2.waitKey(25) & 0xFF == ord('q'):
                     break
-        else:
-            print("Frames Extraction has Finished!" + "\n")
-            break
         
+        else:
+            print("Frames Filtering has Finished!" + "\n")
+            break
     capture.release()
     cv2.destroyAllWindows()
 
 
-print("\n" + "Executing featuresExtraction.py..." + "\n")
 
 #videoPath = input("Hi! Enter the video file name to process please: ")
 videoPath = '../CC5213-Tarea1/comerciales/ballerina.mpg'
-#commFolder = input("\n" + "Now,  enter the folder name that contains the commercials to look for: ")
 
-# Set frame stride
-frameStride = 4
+sobel_threshold = 150
 
-# Resize to 10x10 to reduce quantity of pixels at every frame
-width = 1080
-height = 720
-
-nFramesFromVideo(videoPath, frameStride, width, height)
-
-
-"""
-capture = cv2.VideoCapture(videoPath)
-capture.set(cv2.CAP_PROP_POS_FRAMES, 2)
-print(capture.get(cv2.CAP_PROP_POS_FRAMES))
-success, frame = capture.read()
-frame = cv2.resize(frame, (width, height))
-cv2.imshow('Frame', frame)
-
-time.sleep(3)
-"""
+sobelFilter(videoPath, sobel_threshold)
+print("FIN")
---
+++ featuresExtraction_DEPRECATED.py
@@ -0,0 +1,72 @@
+
+import numpy as np
+import cv2
+import time
+
+def nFramesFromVideo(videoPath, frameStride, width, height):
+    """Return frames resized to width and heigth from a video file every n frames with its timestamps 
+    in miliseconds"""
+
+    print("\n" + "Frames Extraction has Started...")
+    capture = cv2.VideoCapture(videoPath)
+    
+    totalFrames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
+    print("This video file contains " + str(int(totalFrames)) + " frames.")
+    
+    frameCounter = 0
+    while(capture.isOpened()):
+        success, frame = capture.read()
+        
+
+        if success:
+            frameCounter +=1
+            if(frameCounter % frameStride == 0):
+                frame = cv2.resize(frame, (width, height))
+                cv2.imshow('Video', frame)
+                
+                frameTimestamp = "{0:.2f}".format(round(capture.get(0),2))
+                frameIndex = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
+                print(str(frameIndex) + " " + frameTimestamp)
+
+                #return "Hola!"
+
+                # Normal Speed cv2.waitKey(25) 
+                # Max Speed     cv.waitKey(1)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+        else:
+            print("Frames Extraction has Finished!" + "\n")
+            break
+        
+    capture.release()
+    cv2.destroyAllWindows()
+
+
+print("\n" + "Executing featuresExtraction.py..." + "\n")
+
+#videoPath = input("Hi! Enter the video file name to process please: ")
+videoPath = '../CC5213-Tarea1/comerciales/ballerina.mpg'
+#commFolder = input("\n" + "Now,  enter the folder name that contains the commercials to look for: ")
+
+# Set frame stride
+frameStride = 4
+
+# Resize to 10x10 to reduce quantity of pixels at every frame
+width = 1080
+height = 720
+
+#nFramesFromVideo(videoPath, frameStride, width, height)
+
+div = 0/4
+print(div)
+
+"""
+capture = cv2.VideoCapture(videoPath)
+capture.set(cv2.CAP_PROP_POS_FRAMES, 2)
+print(capture.get(cv2.CAP_PROP_POS_FRAMES))
+success, frame = capture.read()
+frame = cv2.resize(frame, (width, height))
+cv2.imshow('Frame', frame)
+
+time.sleep(3)
+"""
