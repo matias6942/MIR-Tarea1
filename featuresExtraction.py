ace266850b2ca5d04f6ef8a94debf98be3bf639b
matias6942 <matias.zamora@ing.uchile.cl>
2018-09-30 16:51:59 -0300

    Resizing to 10x10

--- featuresExtraction.py
+++ featuresExtraction.py
@@ -1,13 +1,14 @@
 
 import numpy as np
 import cv2
+import time
 
-def nFramesFromVideo(videoFile, frameStride, width, height):
+def nFramesFromVideo(videoPath, frameStride, width, height):
     """Return frames resized to width and heigth from a video file every n frames with its timestamps 
     in miliseconds"""
 
     print("\n" + "Frames Extraction has Started...")
-    capture = cv2.VideoCapture(videoFile)
+    capture = cv2.VideoCapture(videoPath)
     
     totalFrames = capture.get(cv2.CAP_PROP_FRAME_COUNT)
     print("This video file contains " + str(int(totalFrames)) + " frames.")
@@ -15,6 +16,8 @@ def nFramesFromVideo(videoFile, frameStride, width, height):
     frameCounter = 0
     while(capture.isOpened()):
         success, frame = capture.read()
+        
+
         if success:
             frameCounter +=1
             if(frameCounter % frameStride == 0):
@@ -24,6 +27,8 @@ def nFramesFromVideo(videoFile, frameStride, width, height):
                 frameTimestamp = "{0:.2f}".format(round(capture.get(0),2))
                 frameIndex = int(capture.get(cv2.CAP_PROP_POS_FRAMES))
                 print(str(frameIndex) + " " + frameTimestamp)
+
+                #return "Hola!"
 
                 # Normal Speed cv2.waitKey(25) 
                 # Max Speed     cv.waitKey(1)
@@ -40,6 +45,7 @@ def nFramesFromVideo(videoFile, frameStride, width, height):
 print("\n" + "Executing featuresExtraction.py..." + "\n")
 
 #videoPath = input("Hi! Enter the video file name to process please: ")
+videoPath = '../CC5213-Tarea1/comerciales/ballerina.mpg'
 #commFolder = input("\n" + "Now,  enter the folder name that contains the commercials to look for: ")
 
 # Set frame stride
@@ -49,6 +55,16 @@ frameStride = 4
 width = 1080
 height = 720
 
-nFramesFromVideo('../CC5213-Tarea1/comerciales/ballerina.mpg', frameStride, width, height)
-        
-        
+nFramesFromVideo(videoPath, frameStride, width, height)
+
+
+"""
+capture = cv2.VideoCapture(videoPath)
+capture.set(cv2.CAP_PROP_POS_FRAMES, 2)
+print(capture.get(cv2.CAP_PROP_POS_FRAMES))
+success, frame = capture.read()
+frame = cv2.resize(frame, (width, height))
+cv2.imshow('Frame', frame)
+
+time.sleep(3)
+"""
